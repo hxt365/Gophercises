166c64f4cda7663bb41083566b34d194c305fd0e
Truong Hoang <truong.hoang@holistics.io>
2021-05-31 23:15:55 +0300

    urlshort

---
+++ urlshort/go.mod
@@ -0,0 +1,5 @@
+module Gophercises/urlshort
+
+go 1.15
+
+require gopkg.in/yaml.v2 v2.4.0
---
+++ urlshort/handler.go
@@ -0,0 +1,69 @@
+package urlshort
+
+import (
+	"gopkg.in/yaml.v2"
+	"net/http"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYml(yml)
+	if err != nil {
+		return nil, err
+	}
+	pathToUrls := mapPathToUrl(pathUrls)
+	return MapHandler(pathToUrls, fallback), nil
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	Url  string `yaml:"url"`
+}
+
+func parseYml(yml []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+	if err := yaml.Unmarshal(yml, &pathUrls); err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+func mapPathToUrl(pathUrls []pathUrl) map[string]string {
+	pathToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathToUrls[pu.Path] = pu.Url
+	}
+	return pathToUrls
+}
